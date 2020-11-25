0a583a3de65b3f1b485cb54b20be3f92980ddd02
Andrey <58406255+4qwerty1@users.noreply.github.com>
2020-11-25 15:15:43 +1000

    create first view by html rendering

--- backend/app1/views.py
+++ backend/app1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from app1.models import SalesOrder
+
+
+def orders_page(request):
+    return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+# обязательные поля: request, template_name
+# template_name - имя html шаблона, который должен быть отрендерен на эту страницу
+# orders - некоторая переменная (словарь)
