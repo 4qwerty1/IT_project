3b59071140dc8a3c8d14400a3cfc9e45380a0471
Andrey <58406255+4qwerty1@users.noreply.github.com>
2020-12-15 00:52:55 +1000

    delete useless files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,9 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'corsheaders',
-    'app1',
-    'products',
     'forms',
 ]
 
