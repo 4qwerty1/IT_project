31e7a2bdf3ef29854f19a04995ed995cd421aaec
hedgehog319 <fign1910@mail.ru>
2020-12-31 10:48:52 +1000

    + Add messages show

--- backend/forms/serializers.py
+++ backend/forms/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 
 from forms.models import User
+from forms.models import Message
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class MessagesSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('id', 'text', 'firstname', 'lastname', 'icon')
