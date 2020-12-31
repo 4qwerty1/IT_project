4f00d230b910c697114e8f7a41e6b855024d59e8
hedgehog319 <fign1910@mail.ru>
2020-12-31 10:48:43 +1000

    + Add messages show

--- backend/forms/urls.py
+++ backend/forms/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from forms.views import UserView
+from forms.views import MessagesView
 
 router = SimpleRouter()
 
 router.register('api/users', UserView)
+router.register('api/messages', MessagesView)
 
 urlpatterns = [
 
