1700bc9c3f6a8235ce234c1f6fbef871182b1dbe
Andrey <58406255+4qwerty1@users.noreply.github.com>
2020-12-31 16:31:58 +1200

    rewrite models

--- backend/new_app/admin.py
+++ backend/new_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from new_app.models import Topic, Message
+
+
+@admin.register(get_user_model())
+class UserAdmin(ModelAdmin):
+    list_display = ('username', 'id', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', )
+
+
+admin.site.register(Topic)
+admin.site.register(Message)
+
--- backend/new_app/models.py
+++ backend/new_app/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    icon = models.ImageField(upload_to='icons', null=True, blank=True)
+
+    def __str__(self):
+        return f'Id {self.id}: {self.username}'
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    data_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Id {self.id}: {self.title}'
+
+
+class Message(models.Model):
+    text = models.CharField(max_length=200)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    # может ссылатьс на удаленного пользователя?
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'From: {self.sender.username}'
+
+    def firstname(self):
+        return self.sender.first_name
+
+    def lastname(self):
+        return self.sender.last_name
+
+    def icon(self):
+        return '../backend' + self.sender.icon.url
