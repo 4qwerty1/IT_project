18107ff0b409e7d6e0666cd413fcb6fc7d48c417
hedgehog319 <fign1910@mail.ru>
2021-01-05 12:17:40 +1000

    * Try to fix main part 3

--- backend/new_app/models.py
+++ backend/new_app/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='icons', null=True, blank=True)
+
+    def __str__(self):
+        return f'Id {self.id}: {self.username}'
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    data_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Id {self.id}: {self.title}'
+
+
+class Message(models.Model):
+    text = models.CharField(max_length=200)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    # может ссылаться на удаленного пользователя?
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'From: {self.sender.username}'
+
+    def get_name(self):
+        return self.sender.get_full_name()
+
+    def get_avatar(self):
+        if self.sender.avatar == '':
+            return None
+        return self.sender.avatar.url
--- backend/new_app/views.py
+++ backend/new_app/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from backend.settings import SECRET_KEY
+from new_app.models import Topic, Message
+from new_app.serializers import UserRegistrationSerializer, TopicSerializer, GetMessages, CreateMessage, \
+    UserProfileSerializer
+import jwt
+
+
+class ListUsers(ListAPIView):
+    queryset = get_user_model().objects.all().filter(is_staff=False)
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ListTopics(ListCreateAPIView):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class GetMessagesView(ListAPIView):
+    serializer_class = GetMessages
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_fields = ['topic']
+
+    def get_queryset(self):
+        if 'topic' not in self.request.query_params:
+            return None
+        else:
+            topic = self.request.query_params['topic']
+            return Message.objects.all().filter(topic=topic).order_by('time_create')
+
+
+class CreateMessageView(CreateAPIView):
+    queryset = Message.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = CreateMessage
+
+
+class LoadNewMessages(ListAPIView):
+    serializer_class = GetMessages
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_fields = ['topic']
+
+    def get_queryset(self):
+        params = self.request.query_params
+        if 'topic' not in params or 'last_message' not in params:
+            return None
+        else:
+            topic = self.request.query_params['topic']
+            start = self.request.query_params['last_message']
+            queryset = Message.objects.all().filter(topic=topic).order_by('time_create')
+
+            # GT >, LT <, GTE >=, LTE <=
+            return queryset.filter(time_create__gt=start)
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        token = self.request.headers['Authorization'].split()[1]
+        user_id = jwt.decode(token, algorithms='HS256', key=SECRET_KEY)['user_id']
+        return get_user_model().objects.get(id=user_id)
+
+    def get(self, request, *args, **kwargs):
+        serializer = UserProfileSerializer(self.get_object())
+
+        user = self.get_object()
+        print(user.first_name)
+        return Response(serializer.data)
+
+    def patch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        serializer = UserProfileSerializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+# request.query_params - параметры после /?  в запросе
+# request.query_params['username'] - пример
